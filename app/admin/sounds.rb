fd59e32cce9b2f6fa2f769bc8880f8919c48ff9a
Ashley Gau <ashley@ashley-mbp.local>
2013-07-22 10:21:44 -0700

    make view page for sound/image display uploaded item

--- app/admin/sounds.rb
+++ app/admin/sounds.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Sound do
-  
+  menu priority: 10
+  show do |f|
+    attributes_table do
+      row :sound do
+        audio_tag f.sound.url, controls: true
+      end
+    end
+    active_admin_comments
+  end
 end
