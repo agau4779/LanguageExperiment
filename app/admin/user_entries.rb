c52c6368572b70e572b96f5e2ec831f8deb5b564
Ashley Gau <ashley@ashley-mbp.local>
2013-07-24 11:03:48 -0700

    add validations and error message to pairs in userentry

--- app/admin/user_entries.rb
+++ app/admin/user_entries.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register UserEntry do
-  menu priority: 2  
+  menu priority: 2
+
+  form do |f|
+    f.inputs 'User Entries' do
+      f.semantic_errors *f.object.errors.keys
+      f.input :iterative_chain
+      f.input :pairs
+    end
+    f.actions
+  end
 end
