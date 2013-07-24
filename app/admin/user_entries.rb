f65e416939d39f66c8afcb3990ce2640bc3194da
Ashley Gau <ashley@ashley-mbp.local>
2013-07-24 16:54:54 -0700

    update user_entry index and view pages

--- app/admin/user_entries.rb
+++ app/admin/user_entries.rb
@@ -1,6 +1,32 @@
 ActiveAdmin.register UserEntry do
   menu priority: 2
 
+  index do
+    column :id
+    column :iterative_chain_id
+    column :created_at
+    column :updated_at
+    default_actions
+  end
+
+  show do |f|
+    attributes_table do
+      row :iterative_chain_id do
+        f.iterative_chain_id
+      end
+      row :pairs do
+        f.pairs
+      end
+      row :created_at do
+        f.created_at
+      end
+      row :updated_at do
+        f.updated_at
+      end
+    end
+    active_admin_comments
+  end
+
   form do |f|
     f.inputs 'User Entries' do
       f.semantic_errors *f.object.errors.keys
