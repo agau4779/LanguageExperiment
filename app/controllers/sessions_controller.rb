b4e8d329154dcf4b8cc1e8aa5040e335ab67a321
Ashley Gau <ashley@ashley-mbp.local>
2013-07-29 10:55:27 -0700

    add rudimentary start page and check for locked chains

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    chain = IterativeChain.available.first
+    if chain==nil
+      flash[:error] = "Sorry- no chains are available at this time. Please try again later."
+      render 'new'
+    else
+      redirect_to training_iterative_chain_path(chain)
+    end
+  end
+
+  def destroy
+  end
 end
