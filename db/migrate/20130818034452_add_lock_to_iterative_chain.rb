9968055bb2385920a1df8ef0cc24d5650ac4f63a
Matt Mo <matt.mo@moovweb.com>
2013-08-18 10:04:20 -0700

    Lock Migration - Fixing mistake in remove_column syntax

--- db/migrate/20130818034452_add_lock_to_iterative_chain.rb
+++ db/migrate/20130818034452_add_lock_to_iterative_chain.rb
@@ -2,6 +2,6 @@ class AddLockToIterativeChain < ActiveRecord::Migration
   def change
     add_column :iterative_chains, :lock_id, :string
     add_column :iterative_chains, :locked_at, :datetime
-    remove_column :iterative_chains, :locked, :boolean
+    remove_column :iterative_chains, :locked
   end
 end
